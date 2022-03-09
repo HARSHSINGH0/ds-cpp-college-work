5833e9e38bdfec8f6497c3f35e03fe52c47f1d56
HARSH SINGH <41187121+HARSHSINGH0@users.noreply.github.com>
2022-03-09 11:21:25 +0530

    priority queue

--- UNIT04/PRG_16.cpp
+++ UNIT04/PRG_16.cpp
@@ -9,90 +9,150 @@
 
 using namespace std;
 
-/* 1.Node Template*/
 class PQNode
 {
 	public:
 		int data;
 		int priority;
-		PQnode *next;
-		
+		PQNode *next;
 };
-/*Queue template*/
-class PQueue{
+
+class PQueue
+{
 	PQNode *front;
+	PQNode *rear;
+	
 	public:
-		PQueue(){
-			front=null;
-			
+		PQueue()
+		{
+			front = NULL;
+			rear = NULL;
 		}
-		void Enqueue(int x);
+		void Enqueue(int x, int p);
 		void Dequeue();
-		void PeekFront();
-		void PeekRear();
 		void Display();
 		int Empty();
 };
-/* Menu */
+
+void PQueue :: Enqueue(int x,int p)
+{
+	//make new node
+	PQNode * t =new PQNode();
+	t->data=x;
+	t->priority=p;
+	t->next=NULL;
+	
+	//first node in pqueue
+	if(front == NULL)
+	{
+		front = t;
+		return;
+	}
+	
+	PQNode *tmp=front;
+	PQNode *prev=NULL;
+	while(tmp && tmp->priority<t->priority)
+	{
+		prev = tmp;
+		tmp = tmp->next;
+	}
+	if(tmp==NULL)//insert at the rear
+	{
+		prev->next=t;
+	}
+	else if(prev==NULL)//t inserted at the  front
+	{
+		t->next=front;
+		front=t;
+	 } 
+	 else//t inserted in the middle
+	 {
+	 	prev->next=t;
+	 	t->next=tmp;
+	 }
+ } 
+
+void PQueue :: Dequeue()
+{
+	if(front==NULL)
+	{
+		cout<<"Underflow....";
+		return;
+	}
+	PQNode *tmp=front;
+	if(front==rear)
+	{
+		front=NULL;
+		rear=NULL;
+	}
+	else
+	{
+		front=front->next;
+	}
+	cout<<"Dequeue Front element is "<<tmp->data<<" with priority : "<<tmp->priority;
+	delete tmp;
+}
+
+void PQueue :: Display()
+{
+	if(front==NULL)
+	{
+		cout<<"Underflow....";
+		return;
+	}
+	else
+	{
+		PQNode * tmp = front;
+		while(tmp)
+		{
+			cout<< tmp->data<<","<<tmp->priority<<"->";
+			tmp = tmp -> next;
+		}
+	}
+}
+
 int main()
 {
-	PQNode d;
-	int num,ch;
+	PQueue p1;
+	int num,ch,p;
 	
 	while(1)
 	{
 		system("cls");
 		
-		cout<<"------------Double Ended Queue------------"<<endl;
-		
-		cout<<"1. Enqueue \n";
-		cout<<"2. Dequeue \n";
-		cout<<"3. Peek in front\n";
-		cout<<"4. Peek in rear\n";
-		cout<<"5.Display\n";
-		cout<<"6.Exit\n";
-		
-		cout<<"Enter your choice: \n";
+		cout<<"1. Enqueue "<<endl;
+		cout<<"2. Dequeue "<<endl;
+		cout<<"3. Display "<<endl;
+		cout<<"Enter th choice : "<<endl;
 		cin>>ch;
-		
 		switch(ch)
 		{
-			case 1:
-				cout<<"Enter element to enqueue: ";
+				case 1:
+				cout<<"Enter a number :\n";
 				cin>>num;
-				d.EnqueueFront(num);
+				cout<<"Enter priority : ";
+				cin>>p;
+				p1.Enqueue(num,p);
 				getch();
 				break;
 				
-			
-				
-			case 2:
-				d.Dequeue();
+				case 2:
+				//cout<<"option 2";
+				p1.Dequeue();
 				getch();
 				break;
 				
-			
-				
-			case 3:
-				d.PeekFront();
+				case 3:
+				//cout<<"option 3";
+				p1.Display();
 				getch();
 				break;
 				
-			case 4:
-				d.PeekRear();
-				getch();
-				break;
-				
-			case 5:
-				d.Display();
-				getch();
-				break;
-				
-			case 6:
+				case 4:
 				exit(1);
-				
 			
-		}//end of switch
-		
-	}//end of while
-}//end of main
+				default:
+				cout<<"Invalid ";
+		}
+	}
+}
