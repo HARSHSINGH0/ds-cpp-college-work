8b228099110c189af15f7b376eee72866683b69f
HARSH SINGH <41187121+HARSHSINGH0@users.noreply.github.com>
2022-03-16 10:03:39 +0530

    circular queue

--- UNIT04/PRG_14.cpp
+++ UNIT04/PRG_14.cpp
@@ -3,96 +3,39 @@
 	Program : Circular Queues
 	Unit : 04
 */
+//circular queue
 #include<iostream>
 #include<conio.h>
+
 #define MAX 4
 using namespace std;
-//2.class Template
+
+//2. Queue template
 class CQueue
 {
 	int A[MAX];
-	int front;
-	int rear;
-	int cnt;
-	
+	int front;//first element
+	int rear;//last element
+	int cnt;//counter element
 	public:
-			CQueue()
-			{
-				front = -1;
-				rear = -1;
-				cnt = 0;
-			}
-			
-			void Enqueue(int x);
-			void Dequeue();
-			void PeekFront();
-			void PeekRear();
-			void Display();
-			int Full();
-			int Empty();
-};//end of class
-
-//3.Functions
-void CQueue :: Enqueue(int x){
-	if(Full())
-	{
-		cout << "Queue Overflow\n";
-		return;
-	}
-	if(front==-1)
-	{
-		front++;
-	}	
-	if(rear == MAX-1)
-	{
-		rear = 0;
-	}
-	else
-	{
-		rear++;
-	}
-	A[rear] = x;
-	cnt++;	
-}//end of enqueue
-void CQueue :: Dequeue(int x){
-	if(Empty()){
-		cout<<"Array Underflow";
-		return;
-	}
-	int tmp=A[front];
-	if(front==rear){
-		front=-1;
-		rear=-1;
-	}
-	else{
-		if(front==MAX-1){
-			front=0;
-		}else{
-			front++;
-		}	
-	}
-	cout<<tmp;
-	cnt--;
-}//dequeue
-void CQueue :: Display(){
-	if(Empty()){
-		cout<<"Array Underflow";
-		return;
-	}
-	j=front;
-	for(i=1;i<=cnt;i++){
-		cout<<A[j]<<" ";
-		if(j==MAX-1){
-			j=0
+		CQueue()
+		{
+			front=-1;
+			rear=-1;
+			cnt=0;
 		}
-		else{
-			j++;
-		}
-	}
-}//display
-int CQueue :: Full()
+		void Enqueue(int x);
+		void Dequeue();
+		void PeekFront();
+		void PeekRear();
+		void Dipslay();
+		int Full();
+		int Empty();
+	
+};//end of OQueue
+int CQueue::Full()
 {
-	if(cnt == MAX)
+	if(cnt==MAX)
 	{
 		return 1;
 	}
@@ -101,9 +44,9 @@ int CQueue :: Full()
 		return 0;
 	}
 }//end of full
-int CQueue :: Empty()
+int CQueue::Empty()
 {
-	if(front == -1)
+	if(cnt==0)
 	{
 		return 1;
 	}
@@ -112,65 +55,144 @@ int CQueue :: Empty()
 		return 0;
 	}
 }//end of empty
-
-
-
-
-//4.Menu
-
+void CQueue::Enqueue(int x)
+{
+	if(Full())
+	{
+		cout<<"Overflow";
+		return;
+	}
+	if(Empty())
+	{
+		front++;
+	}
+	if(rear==MAX-1)
+	{
+		rear=0;
+	}
+	else
+	{
+		rear++;
+	}
+	A[rear]=x;
+	cnt++;
+}
+void CQueue::PeekFront()
+{
+	if(Empty())
+	{
+		cout<<"Underflow";
+		return;
+	}
+	cout<<"Peek Front: "<<A[front];
+	
+}//end of peekFront
+void CQueue::PeekRear()
+{
+	if(Empty())
+	{
+		cout<<"Underflow";
+		return;
+	}
+	cout<<"Peek Rear: "<<A[rear];
+	
+}
+void CQueue::Dequeue()
+{
+	if(Empty())
+	{
+		cout<<"Underflow";
+		return;
+	}
+		int tmp=A[front];
+		if(front==rear)//single element
+		{
+			front=-1;//no elements 
+			rear=-1;
+		}
+		else
+		{
+			if(front==MAX-1)//if front is at last element
+			{
+				front=0;//set front =0
+			}
+			else
+			{
+				front++;//sets next element as front
+			}
+		}
+		cout<<"Dequeue: "<<tmp;
+		cnt--;
+	
+}//end of dequeue
+void CQueue::Dipslay()
+{
+	if(Empty())
+	{
+		cout<<"underflow";
+	}
+	int j=front;
+	for(int i=1;i<=cnt;i++)
+	{
+		cout<<A[j]<<" ";
+		if(j==MAX-1)
+		{
+			j=0;
+		}
+		else
+		{
+			j++;
+		}//end of else
+	}//end of for
+}
 int main()
 {
 	CQueue c;
-	int num,ch;
-	
+	int num;
+	int ch;
 	while(1)
 	{
 		system("cls");
+		cout<<"* Circular Queue *"<<endl;
+		cout<<"1.Enqueue"<<endl;
+		cout<<"2.Dequeue"<<endl;
+		cout<<"3.Peek Front"<<endl;
+		cout<<"4.Peek Rare"<<endl;
+		cout<<"5.Display"<<endl;
+		cout<<"6.Exit"<<endl;
 		
-		cout << "*****Circular Queues*****\n";
-		
-		cout << "1.Enqueue an Element\n";
-		cout << "2.Dequeue an Element\n";
-		cout << "3.Peek at the front in Qeuue\n";
-		cout << "4.Peek at the rear in Queue\n";
-		cout << "5.Display the Queue\n";
-		cout << "6.Exit\n\n";
-		
-		cout << "Enter Your Choice : ";
-		cin >> ch;
-		
+		cout<<"Enter the choice: ";
+		cin>>ch;
 		switch(ch)
 		{
 			case 1:
-				cout << "Enter an element : ";
-				cin >> num;
-				c.Enqueue(num);
-				getch();
-				break;
+				   cout<<"Enter the number: ";
+				   cin>>num;
+				   c.Enqueue(num);
+				   getch();
+				   break;
 			case 2:
-				
-				getch();
-				break;
+				   c.Dequeue();
+				   getch();
+				   break;
 			case 3:
-				//c.PeekFront();
-				getch();
-				break;
+				   c.PeekFront();
+				   getch();
+				   break;
 			case 4:
-				//c.PeekRear();
-				getch();
-				break;
+				   c.PeekRear();
+				   getch();
+				   break;
 			case 5:
-				cout << "Displaying the Queue : ";
-				//c.Display();
-				getch();
-				break;
+				   c.Dipslay();
+				   getch();
+				   break;
 			case 6:
-				cout << "Program is Closed.";
-				exit(1);
+				   exit(1);
 			default:
-				cout << "Incorrect Choice.";
-				getch();
-				break;
-		}
-	}
-}//end of menu
+				   cout<<"Incorrect choice"<<endl;
+				   getch();
+				   break;
+		}//end of switch
+	}//end of while
+}//end of main
